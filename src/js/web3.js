e45c8fdfbed2b83265b3d5b34554efd0a83781a5
gabsrps <gabriel.santos@sou.inteli.edu.br>
2023-04-06 15:46:06 -0300

    comentários no código do front

--- src/js/web3.js
+++ src/js/web3.js
@@ -13,329 +13,348 @@
 
         const connectContract = async () => {
             const ABI = [
-	{
-		"inputs": [
-			{
-				"internalType": "bytes15",
-				"name": "_imei",
-				"type": "bytes15"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_valorAtivo",
-				"type": "uint256"
-			},
-			{
-				"internalType": "address payable",
-				"name": "_endereco",
-				"type": "address"
-			}
-		],
-		"name": "adicionarUsuarios",
-		"outputs": [],
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address payable",
-				"name": "_endereco",
-				"type": "address"
-			}
-		],
-		"name": "depositarInicial",
-		"outputs": [],
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address payable",
-				"name": "_endereco",
-				"type": "address"
-			}
-		],
-		"name": "entrarGrupo",
-		"outputs": [],
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "renovarContrato",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_nomeGrupo",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_minimoParticipantes",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_maximoParticipantes",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_taxAdmin",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_duracaoDias",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "participante",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "valor",
-				"type": "uint256"
-			}
-		],
-		"name": "novoParticipanteGrupo",
-		"type": "event"
-	},
-	{
-		"inputs": [],
-		"name": "reservaRisco",
-		"outputs": [],
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_amount",
-				"type": "uint256"
-			}
-		],
-		"name": "withdraw",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "dataInicio",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "dataValidade",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "duracaoDias",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "maximoParticipantes",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "meuGrupo",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "nomeGrupo",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "saldoTotal",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "qntparticipantes",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "minimoParticipantes",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "maximoParticipantes",
-				"type": "uint256"
-			},
-			{
-				"internalType": "address payable",
-				"name": "seguradora",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "taxAdmin",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "dataInicio",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "dataValidade",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "duracaoDias",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "nomeGrupo",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "qntParticipantes",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "quantidadeParticipantes",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "_endereco",
-				"type": "address"
-			}
-		],
-		"name": "saldoParticipante",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "saldoTotalGrupo",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "taxaAdmin",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	}
-];
+				{
+					"inputs": [
+						{
+							"internalType": "string",
+							"name": "_nomeGrupo",
+							"type": "string"
+						},
+						{
+							"internalType": "uint256",
+							"name": "_minimoParticipantes",
+							"type": "uint256"
+						},
+						{
+							"internalType": "uint256",
+							"name": "_maximoParticipantes",
+							"type": "uint256"
+						},
+						{
+							"internalType": "uint256",
+							"name": "_taxAdmin",
+							"type": "uint256"
+						},
+						{
+							"internalType": "uint256",
+							"name": "_duracaoDias",
+							"type": "uint256"
+						}
+					],
+					"stateMutability": "nonpayable",
+					"type": "constructor"
+				},
+				{
+					"anonymous": false,
+					"inputs": [
+						{
+							"indexed": true,
+							"internalType": "address",
+							"name": "participante",
+							"type": "address"
+						},
+						{
+							"indexed": false,
+							"internalType": "uint256",
+							"name": "valor",
+							"type": "uint256"
+						}
+					],
+					"name": "novoParticipanteGrupo",
+					"type": "event"
+				},
+				{
+					"inputs": [
+						{
+							"internalType": "bytes15",
+							"name": "_imei",
+							"type": "bytes15"
+						},
+						{
+							"internalType": "uint256",
+							"name": "_valorAtivo",
+							"type": "uint256"
+						},
+						{
+							"internalType": "address payable",
+							"name": "_endereco",
+							"type": "address"
+						}
+					],
+					"name": "adicionarUsuarios",
+					"outputs": [],
+					"stateMutability": "payable",
+					"type": "function"
+				},
+				{
+					"inputs": [],
+					"name": "dataInicio",
+					"outputs": [
+						{
+							"internalType": "uint256",
+							"name": "",
+							"type": "uint256"
+						}
+					],
+					"stateMutability": "view",
+					"type": "function"
+				},
+				{
+					"inputs": [],
+					"name": "dataValidade",
+					"outputs": [
+						{
+							"internalType": "uint256",
+							"name": "",
+							"type": "uint256"
+						}
+					],
+					"stateMutability": "view",
+					"type": "function"
+				},
+				{
+					"inputs": [
+						{
+							"internalType": "address payable",
+							"name": "_endereco",
+							"type": "address"
+						}
+					],
+					"name": "depositarInicial",
+					"outputs": [],
+					"stateMutability": "payable",
+					"type": "function"
+				},
+				{
+					"inputs": [],
+					"name": "duracaoDias",
+					"outputs": [
+						{
+							"internalType": "uint256",
+							"name": "",
+							"type": "uint256"
+						}
+					],
+					"stateMutability": "view",
+					"type": "function"
+				},
+				{
+					"inputs": [
+						{
+							"internalType": "address payable",
+							"name": "_endereco",
+							"type": "address"
+						}
+					],
+					"name": "entrarGrupo",
+					"outputs": [],
+					"stateMutability": "payable",
+					"type": "function"
+				},
+				{
+					"inputs": [
+						{
+							"internalType": "address",
+							"name": "_endereco",
+							"type": "address"
+						}
+					],
+					"name": "indenizacao",
+					"outputs": [
+						{
+							"internalType": "uint256",
+							"name": "",
+							"type": "uint256"
+						}
+					],
+					"stateMutability": "view",
+					"type": "function"
+				},
+				{
+					"inputs": [],
+					"name": "maximoParticipantes",
+					"outputs": [
+						{
+							"internalType": "uint256",
+							"name": "",
+							"type": "uint256"
+						}
+					],
+					"stateMutability": "view",
+					"type": "function"
+				},
+				{
+					"inputs": [],
+					"name": "meuGrupo",
+					"outputs": [
+						{
+							"internalType": "string",
+							"name": "nomeGrupo",
+							"type": "string"
+						},
+						{
+							"internalType": "uint256",
+							"name": "saldoTotal",
+							"type": "uint256"
+						},
+						{
+							"internalType": "uint256",
+							"name": "qntparticipantes",
+							"type": "uint256"
+						},
+						{
+							"internalType": "uint256",
+							"name": "minimoParticipantes",
+							"type": "uint256"
+						},
+						{
+							"internalType": "uint256",
+							"name": "maximoParticipantes",
+							"type": "uint256"
+						},
+						{
+							"internalType": "address payable",
+							"name": "seguradora",
+							"type": "address"
+						},
+						{
+							"internalType": "uint256",
+							"name": "taxAdmin",
+							"type": "uint256"
+						},
+						{
+							"internalType": "uint256",
+							"name": "dataInicio",
+							"type": "uint256"
+						},
+						{
+							"internalType": "uint256",
+							"name": "dataValidade",
+							"type": "uint256"
+						},
+						{
+							"internalType": "uint256",
+							"name": "duracaoDias",
+							"type": "uint256"
+						}
+					],
+					"stateMutability": "view",
+					"type": "function"
+				},
+				{
+					"inputs": [],
+					"name": "nomeGrupo",
+					"outputs": [
+						{
+							"internalType": "string",
+							"name": "",
+							"type": "string"
+						}
+					],
+					"stateMutability": "view",
+					"type": "function"
+				},
+				{
+					"inputs": [],
+					"name": "qntParticipantes",
+					"outputs": [
+						{
+							"internalType": "uint256",
+							"name": "",
+							"type": "uint256"
+						}
+					],
+					"stateMutability": "view",
+					"type": "function"
+				},
+				{
+					"inputs": [],
+					"name": "quantidadeParticipantes",
+					"outputs": [
+						{
+							"internalType": "uint256",
+							"name": "",
+							"type": "uint256"
+						}
+					],
+					"stateMutability": "view",
+					"type": "function"
+				},
+				{
+					"inputs": [],
+					"name": "renovarContrato",
+					"outputs": [],
+					"stateMutability": "nonpayable",
+					"type": "function"
+				},
+				{
+					"inputs": [],
+					"name": "reservaRisco",
+					"outputs": [],
+					"stateMutability": "payable",
+					"type": "function"
+				},
+				{
+					"inputs": [
+						{
+							"internalType": "address",
+							"name": "_endereco",
+							"type": "address"
+						}
+					],
+					"name": "saldoParticipante",
+					"outputs": [
+						{
+							"internalType": "uint256",
+							"name": "",
+							"type": "uint256"
+						}
+					],
+					"stateMutability": "view",
+					"type": "function"
+				},
+				{
+					"inputs": [],
+					"name": "saldoTotalGrupo",
+					"outputs": [
+						{
+							"internalType": "uint256",
+							"name": "",
+							"type": "uint256"
+						}
+					],
+					"stateMutability": "view",
+					"type": "function"
+				},
+				{
+					"inputs": [],
+					"name": "taxaAdmin",
+					"outputs": [
+						{
+							"internalType": "uint256",
+							"name": "",
+							"type": "uint256"
+						}
+					],
+					"stateMutability": "view",
+					"type": "function"
+				},
+				{
+					"inputs": [
+						{
+							"internalType": "uint256",
+							"name": "_amount",
+							"type": "uint256"
+						}
+					],
+					"name": "withdraw",
+					"outputs": [],
+					"stateMutability": "nonpayable",
+					"type": "function"
+				}
+			];
 
-            const Address = "0x56162319Fe9FB62281C13A9dAa8Bb0FA16fEBb35";
+            const Address = "0x7A07BEb0ae2de2a2254e52Bd3B2BEb9D3F2ac827";
             window.web3 = await new Web3(window.ethereum);
             window.contract = await new window.web3.eth.Contract(ABI, Address);
             document.getElementById("contractArea").innerHTML = "Connected to Contract";
